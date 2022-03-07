09b3750e304226a64c6703a851386a3d31d16f60
Aleksei <TheEclipsE@tut.by>
2022-03-07 22:23:13 +0300

    Add create, userviewmodel add repository

---
+++ TestProjectWeb/Data/Repository.cs
@@ -0,0 +1,19 @@
+﻿using TestProjectWeb.Data.DbModels;
+
+namespace TestProjectWeb.Data
+{
+    public class Repository
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public Repository(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<User> GetAll()
+        {
+            return _dbContext.Users.ToList();
+        }
+    }
+}
