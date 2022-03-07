002da0767558b91f5b60dfccf853e342704ed6ac
Aleksei <TheEclipsE@tut.by>
2022-03-07 15:29:15 +0300

    add databes

---
+++ TestProjectWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestProjectWeb.Data.DbModels;
+using TestProjectWeb.Models;
+
+namespace TestProjectWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
+ 
