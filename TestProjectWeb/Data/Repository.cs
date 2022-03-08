2f6bdcf4523ab1ee1e0bca8716769625e22dd6b2
Aleksei <TheEclipsE@tut.by>
2022-03-08 19:58:29 +0300

    AddChanges

--- TestProjectWeb/Data/Repository.cs
+++ TestProjectWeb/Data/Repository.cs
@@ -4,7 +4,7 @@ namespace TestProjectWeb.Data
 {
     public class Repository
     {
-        private readonly ApplicationDbContext _dbContext;
+        private ApplicationDbContext _dbContext;
 
         public Repository(ApplicationDbContext dbContext)
         {
@@ -14,6 +14,23 @@ namespace TestProjectWeb.Data
         public List<User> GetAll()
         {
             return _dbContext.Users.ToList();
+        }
+
+        public User GetById(int id)
+        {
+            return _dbContext.Users.Where(user => user.Id == id).FirstOrDefault();
+        }
+
+        public void AddUser(User user)
+        {
+            _dbContext.Users.Add(user);
+            _dbContext.SaveChanges();
+        }
+
+        public void DeleteUser(User user)
+        {
+            _dbContext.Users.Remove(user);
+            _dbContext.SaveChanges();
         }
     }
 }
