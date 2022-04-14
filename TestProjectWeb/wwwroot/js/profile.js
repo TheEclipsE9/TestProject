e2e15a4cf09acb02375b77f1db74df221792b5dc
Aleksei <TheEclipsE@tut.by>
2022-04-14 20:01:03 +0300

    done

--- TestProjectWeb/wwwroot/js/profile.js
+++ TestProjectWeb/wwwroot/js/profile.js
@@ -1,18 +1,18 @@
 ﻿$(document).ready(function () {
-    $('.link-options-open').click(function () {
+    $('.options-open').click(function () {
         console.log("show!")
         $('.profile-options').show();
     });
-    $('.link-options-close').click(function () {
+    $('.options-close').click(function () {
         console.log("close!")
         $('.profile-options').hide();
     });
 
-    $('.link-deleteAccount-open').click(function () {
+    $('.deleteAccount-open').click(function () {
         console.log("show!")
         $('.delete-account').show();
     });
-    $('.link-deleteAccount-close').click(function () {
+    $('.deleteAccount-close').click(function () {
         console.log("close!")
         $('.delete-account').hide();
     });
